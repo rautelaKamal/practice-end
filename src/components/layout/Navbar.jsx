9dd312e0a8b5860e87dacc2134e8fe4291c8d262
Your Name <your.email@example.com>
2025-12-27 03:25:02 +0530

    navbar: menu updated

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,15 +1,16 @@
-
+import { useState } from "react";
 
 const Navbar = () => {
+const [open,setOpen] = useState(false);
   return (
      <div className="w-full">
-    <div className="mx-6 flex justify-between px-6 py-5 md:px-12 " >
+    <div className="flex justify-between px-3 py-5 lg:px-12 " > 
       <div className="">
         kartik
       </div>
 
-
-      <div className="flex items-center gap-6">
+     
+      <div className="hidden lg:flex gap-6">
       <div>
         proof-of-work
       </div>
@@ -20,9 +21,22 @@ const Navbar = () => {
         light switch
       </div>
 </div>
+    <button 
+    className="lg:hidden"
+    onClick={()=> setOpen(!open)}>☰</button>
+    
+    {open && (
+  <div className="md:hidden mx-3 mt-2 rounded-lg border p-4 flex flex-col gap-4">
+    <div>proof-of-work</div>
+    <div>blogs</div>
+    <div>light switch</div>
+  </div>
+)}
+
     </div>
     </div>
   )
 }
+
 
 export default Navbar;
